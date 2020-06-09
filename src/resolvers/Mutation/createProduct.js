46db1d790daacd08d93b8ab56726af51ec2208af
alihasana <hasana.ali@gmail.com>
2020-06-09 14:54:04 +0200

    validation for createProduct

--- src/resolvers/Mutation/createProduct.js
+++ src/resolvers/Mutation/createProduct.js
@@ -1,15 +1,18 @@
 import {Product} from "../../models/Product";
+import {CreateProductValidator} from "../../schemas";
+import { UserInputError } from "apollo-server-express"
 
 
-export default async (_, {
-    name,
-    description,
-    url,
-    quantity,
-    price,
-    vat,
-    associationId
-}) => {
+export default async (_, {name, description, url, quantity, price, vat, associationId}) => {
+    // Validation
+    const validate = CreateProductValidator.validate({name, description, url, quantity, price, vat, associationId},
+        {abortEarly: false});
+    if(validate.error) {
+        throw  new UserInputError( "Please provide the correct input data", {
+            validationError : validate.error.details
+        } );
+
+    }
     const product = new Product({
         name,
         description,
