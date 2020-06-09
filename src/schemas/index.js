4978c4d87ccefca918b54f185e8905822da19e3d
alihasana <hasana.ali@gmail.com>
2020-06-09 14:55:07 +0200

    type error

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,3 +1,3 @@
 export {default as LoginValidator} from './login'
 export {default as CreateSaleValidator} from './createSale'
-export {default as CreateProductValidator} from './crerateProduct'
+export {default as CreateProductValidator} from './createProduct'
