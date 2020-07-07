ad61b05eba70ce0f012f53807e2cd5330570db20
alihasana <hasana.ali@gmail.com>
2020-07-07 20:39:31 +0200

    verification of shopping carte header and others

--- src/middleware/isAuth.js
+++ src/middleware/isAuth.js
@@ -5,33 +5,41 @@ import knex from "../sql";
 
 module.exports = async (req, res, next) => {
     const authHeader = req.get('Authorization')
-    if(!authHeader)  return returnNull(req, res, 'Unauthenticated');
-    const token = authHeader.split(' ');
-    if (token.length !== 2 || token[0] !== 'Bearer' || token[1] === '') return returnNull(req, res,'Error in token format');
-    let decodedToken;
-    let publicKey = fs.readFileSync(path.resolve(__dirname + '../../../jwt/public.key'), 'utf8'); // get data as string content
-    try {
-    decodedToken = jwt.verify(token[1], publicKey, {
-        algorithm: 'RS256'
-    });
-    } catch (e) {
-        return returnNull(req, res,'Unauthenticated: Error while verification');
+    if (!authHeader) {
+        const cartHeader = req.get('Shopping-Cart')
+        if (!cartHeader || cartHeader !== 'associationManager') {
+            res.status(401);
+            res.send({'message':'Unauthenticated Access'})
+            return res
+        }
+        return returnNull(req, next);
     }
-    if (!decodedToken) return returnNull(req, res,'Unauthenticated: Token is not authenticated');
+    const token = authHeader.split(' ');
+    if (token.length !== 2 || token[0] !== 'Bearer' || token[1] === '') return returnNull(req, next,);
+    let decodedToken;
+    let publicKey = fs.readFileSync(
+        path.resolve(
+            __dirname + '../../../jwt/public.key'),
+        'utf8'
+    ); // get data as string content
+    try {
+        decodedToken = jwt.verify(token[1], publicKey, {
+            algorithm: 'RS256'
+        });
+    } catch (e) {
+        return returnNull(req, next);
+    }
+    if (!decodedToken) return returnNull(req, next);
     req.isAuth = true;
     req.userEmail = decodedToken.username;
     req.userRole = decodedToken.roles;
-    console.log(req);
-
-    let email = await  knex('user').select('email')
-        .where({ email: decodedToken.username })
-    if(email[0] === undefined) return returnNull(req, res, 'Unauthenticated: Not a valid user');;
+    let email = await knex('user').select('email')
+        .where({email: decodedToken.username})
+    if (email[0] === undefined) return returnNull(req, next);
     next();
 }
 
-let returnNull = (req, res, message) => {
+let returnNull = (req, next) => {
     req.isAuth = false;
-    res.status(401)
-    res.send({message})
-    return res;
+    return next();
 }
